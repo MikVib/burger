ef49aabd131b58a7c808ce443ca10d5e35fad267
MikVib <kmgtmx@gmail.com>
2022-12-22 20:14:57 +0600

    first commit

---
+++ js/modal.js
@@ -0,0 +1,32 @@
+
+
+    const coverBtn = document.querySelector('.cover__button')
+    const backdropModal = document.querySelector('.backdrop')
+    const modal = document.querySelector('.modal')
+    const modalCloseButton = document.querySelector('.modalCloseButton')
+    const thankYou = document.querySelector('.thankYou')
+
+    thankYou.innerHTML=`
+        <h2>THANK YOU</h2>
+        <p>Out operators call you back</p>
+    `
+
+
+    coverBtn.addEventListener('click',()=>{
+        backdropModal.classList.add('backdropActive')
+        modal.classList.add('modalActive')
+    })
+
+    modalCloseButton.addEventListener('click',()=>{
+        backdropModal.classList.remove('backdropActive')
+        modal.classList.remove('modalActive')
+    })
+
+    backdropModal.addEventListener('click',()=>{
+        backdropModal.classList.remove('backdropActive')
+        modal.classList.remove('modalActive')
+    })
+
+    modal.addEventListener('click',(e)=>{
+       e.stopPropagation()
+    })
